272b7929f35b507f033034a9700f1ad4f38ecd60
Ahmed-Khan-2016 <ahmed.kahn@mywidz.se>
2023-03-16 15:48:01 +0100

    first  commit

---
+++ src/main/java/com/asab/pipline/test/PiplineJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.asab.pipline.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiplineJenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiplineJenkinsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asab/pipline/test/PiplineJenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asab.pipline.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiplineJenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
